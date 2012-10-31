f4d0fb232bc2cbfa7d29bdc727354ee11da3e125
saku39 <zukezuke.39@gmail.com>
2012-11-01 01:44:21 +0900

    comment out password

--- apps/config.php
+++ apps/config.php
@@ -3,6 +3,6 @@
 // DB
 define('DSN', 'mysql:host=localhost;dbname=mogabe');
 define('DB_USER', 'jisenare');
-define('DB_PASSWORD', 'GhKVxeO');
+define('DB_PASSWORD', '*****');
 
 define('SITE_URL', 'http://www5374uf.sakura.ne.jp/');
